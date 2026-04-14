8663002b6612eadbd8be6cd6d51227d3796bee38
UniversityAccount2024 <u2484105@uel.ac.uk>
2026-04-14 23:37:00 +0100

    Altered src Main. Wrapped entire app in a provider so that every page knows if a wallet is connected (Connecting wallet in future commit)

--- PetAd-Frontend-main/src/main.tsx
+++ PetAd-Frontend-main/src/main.tsx
@@ -1,13 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 1. Import the Router
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter> {/* 2. Wrap your App */}
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+// Web3 Imports
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { mainnet, polygon, sepolia } from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: 'PetAd',
+  projectId: 'YOUR_PROJECT_ID', // TODO: Get one for free at cloud.walletconnect.com
+  chains: [mainnet, polygon, sepolia],
+  ssr: true, 
+});
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
+);
